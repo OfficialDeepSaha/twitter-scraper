48072370f872c1dc41275a4d439fa9ea6f54c15a
unknown <deepsaha01896@gmail.com>
2025-04-14 17:48:04 +0530

    fixing port size error

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from app import app, db
 import models  # Import to ensure models are registered with SQLAlchemy
 
@@ -6,4 +7,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Get port from environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
